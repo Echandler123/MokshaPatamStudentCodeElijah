1e3ac70e6dfb1a6818b4c6deea9e9337ca7833d4
Echandler123 <elijah.chandler@menloschool.org>
2024-09-05 23:12:45 -0700

    update

--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Moksha Patam
  * A puzzle created by Zach Blick
@@ -14,15 +17,67 @@ public class MokshaPatam {
      * TODO: Complete this function, fewestMoves(), to return the minimum number of moves
      *  to reach the final square on a board with the given size, ladders, and snakes.
      */
-    public static int fewestMoves(int boardsize, int[][] ladders, int[][] snakes) {
-        int bestmove = 0;
-        for (int i = 0; i < ladders.length; i++) {
-            for (int j = 0; j < ladders[i].length; j++) {
-                int move = ladders[i][j];
-                if(bestmove  < boardsize - move);
-                    bestmove = move;
+    public int fewestMoves(int boardsize, int[][] ladders, int[][] snakes) {
+        int moves = 0;
+        Node nodeone = new Node();
+        Queue<Node> myQueue = new LinkedList<Node>();
+        myQueue.add(nodeone);
+        int num = 1;
+        for(int i = 1; i < boardsize;i++);{
+            num++;
+            myQueue.add(new Node(num));
+        }
+        while(myQueue.size() > 0){
+            moves ++;
+            Node Currentnode = myQueue.remove();
+            if(Currentnode.getNodeup().visited == false){
+                myQueue.add(Currentnode.getNodeup());
+            }
+            if(Currentnode.getNodedown().visited == false){
+                myQueue.add(Currentnode.getNodedown());
+            }
+            if(Currentnode.getVal() == boardsize);{
+                return moves;
             }
         }
         return 0;
+    }
+    public class Node{
+        private int val;
+        private Node nodeup;
+        private Node nodedown;
+        private boolean visited;
+        public Node(){
+            val = 1;
+        }
+        public Node(int val){
+            this.val = val;
+        }
+
+        public void setNodedown(Node nodedown) {
+            this.nodedown = nodedown;
+        }
+
+        public void setNodeup(Node nodeup) {
+            this.nodeup = nodeup;
+        }
+
+        public Node getNodedown() {
+            return nodedown;
+        }
+
+        public Node getNodeup() {
+            return nodeup;
+        }
+
+        public int getVal() {
+            return val;
+        }
+
+
+
+        public boolean isVisited() {
+            return visited;
+        }
     }
 }
